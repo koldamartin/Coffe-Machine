a19812b08388f3c3220d296e8b646c5ed22c5de8
Jan Kolda <kolda.jan@feg.eu>
2022-12-08 10:48:41 +0100

    el/if nahrazeno case/match

--- main.py
+++ main.py
@@ -71,24 +71,27 @@ def main():
     profit_made = 0
     machine_on = True  # pouzil bych vystiznejsi nazev, aby bylo jasno, co ta condition dela.
     while machine_on:  # to  == True je tu zbytecny.
-        ask_question = input("What would you like? espresso/latte/cappuccino (or type 'report' or 'off'): ")
-        if ask_question == "off":  # tady by bylo vhodnejsi pouzit konstrukci case - match misto if-elif, ktera je v pythonu od verze 3.10
-            machine_on = False
-        elif ask_question == "report":
-            print(
-                f"Water: {resources['water']} ml \nMilk: {resources['milk']} ml \nCoffee: {resources['coffee']} g \nMoney: ${profit_made}")
-        elif ask_question in MENU.keys():
-            if check_resources(ask_question):
-                print("Please insert coins: ")
-                # type by mohlo bejt asi i int, protoze jsou to cele mince
-                quarters = int(input("How many quarters?: "))
-                dimes = int(input("How many dimes?: "))
-                nickles = int(input("How many nickles?: "))
-                pennies = int(input("How many pennies?: "))
-                money = round(quarters * 0.25 + dimes * 0.1 + nickles * 0.05 + pennies * 0.01, 2)
-                if check_money(ask_question, money):
-                    check_leftovers(ask_question)
-                    profit_made += profit(ask_question)
+        machine_action = input(
+            "What would you like? espresso/latte/cappuccino (or type 'report' or 'off'): ")  # vystiznejsi nazev
+        match machine_action:
+            case "off":  # tady by bylo vhodnejsi pouzit konstrukci case - match misto if-elif, ktera je v pythonu od verze 3.10
+                machine_on = False
+            case "report":
+                print(f"Water: {resources['water']} ml \nMilk: {resources['milk']} ml \nCoffee: {resources['coffee']} g \nMoney: ${profit_made}")
+            # case 'espresso' | 'latte' | 'cappuccino':
+            case drink if drink in MENU.keys():
+                if check_resources(machine_action):
+                    print("Please insert coins: ")
+                    # type by mohlo bejt asi i int, protoze jsou to cele mince
+                    # je celkem elegantni z toho udelat dictionary
+                    inserted_coins = {'quarters': {'value': 0.25, 'count': int(input("How many quarters?: "))},
+                                      'dimes': {'value': 0.1, 'count': int(input("How many dimes?: "))},
+                                      'nickles': {'value': 0.05, 'count': int(input("How many nickles?: "))},
+                                      'pennies': {'value': 0.01, 'count': int(input("How many pennies?: "))}}
+                    money = round(sum(v['value'] * v['count'] for v in inserted_coins.values()), 2)
+                    if check_money(machine_action, money):
+                        check_leftovers(machine_action)
+                        profit_made += profit(machine_action)
 
 
 if __name__ == "__main__":
